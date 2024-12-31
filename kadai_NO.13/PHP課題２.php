ef3720592a67589797276420886e922fe91f82ef
Teramura Takumi <tyc.soccer0602@gmail.com>
2024-12-31 12:19:02 +0000

    課題NO.13を提出しました。

---
+++ kadai_NO.13/PHP課題２.php
@@ -0,0 +1,6 @@
+<?php
+$total = 0 ;
+for($i = 1; $i < 10001 ; $i++){
+    $total += $i;
+}
+echo $total ;
---
+++ kadai_NO.13/PHP課題３.php
@@ -0,0 +1,6 @@
+<?php
+$fruits = array("みかん","りんご","ぶどう","もも","いちご");
+foreach($fruits as $fruit){
+    echo $fruit ;
+    echo "\n";
+}
