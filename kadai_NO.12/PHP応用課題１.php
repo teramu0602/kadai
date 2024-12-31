9f6a7eaf31c9740a6d0aea52950568c35fcf0772
Teramura Takumi <tyc.soccer0602@gmail.com>
2024-12-31 10:20:04 +0000

    課題NO.１２を提出しました。

---
+++ kadai_NO.12/PHP応用課題１.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(E_ALL); ini_set('display_errors', '1'); 
+$calender_2018 = [
+    "January" => "１月",
+    "February" => "2月",
+    "March" => "3月",
+    "April" => "4月",
+    "May" => "5月",
+    "June" => "6月",
+    "July" => "7月",
+    "August" => "8月",
+    "September" => "9月",
+    "October" => "10月",
+    "November" => "11月",
+    "December" => "12月"
+];
+
+echo $calender_2018["December"];
---
+++ kadai_NO.12/PHP課題1-3.php
@@ -0,0 +1,5 @@
+<?php
+$hello = "Hello,";
+$name = "Teramura";
+$world = "'s World!";
+echo $hello.=$name.=$world;
