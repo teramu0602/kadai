d10434627b45a2d1bc60292fa26ed8f73630919d
Teramura Takumi <tyc.soccer0602@gmail.com>
2025-01-03 10:34:24 +0000

    課題NO.17を提出しました。

---
+++ kadai_NO.17/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charaset="UTF-8">
+        <meta http-eqiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>Blade Create</h1>
+    </body>
+</html>
