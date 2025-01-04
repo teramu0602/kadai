7de56e89169840fb73876a1e43668af6f4a8792a
Teramura Takumi <tyc.soccer0602@gmail.com>
2025-01-04 09:02:40 +0000

    課題NO.18を提出しました。

---
+++ kadai_NO.18/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+@section('title',"MyProfile")
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>  
+@endsection
