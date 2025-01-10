2e8c84eeb2ada70a26f461a98228c1f46ff2e894
Teramura Takumi <tyc.soccer0602@gmail.com>
2025-01-10 11:18:33 +0000

    課題NO.21を提出しました。

---
+++ kadai_NO.21/ProfileController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Profile;
+
+class ProfileController extends Controller
+{
+    //
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+
+        return redirect('admin/profile/create');
+    }
+
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
+}
